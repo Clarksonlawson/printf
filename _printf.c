74a9d6d87120421924a5d4175c6e09b18e4020ad
Clarkson <clarksonlawson1@gmail.com>
2023-10-16 23:28:02 -0700

    latest

--- _printf.c
+++ _printf.c
@@ -1,55 +1,5 @@
 #include "main.h"
-#include <stddef.h>
 #include <stdarg.h>
-
-/**
- * print_str - Helper function to print a string
- * @str: String to print
- * @count: Pointer to the count of printed characters
- */
-void print_str(const char *str, int *count)
-{
-	if (str == NULL)
-		str = "(null)";
-
-	while (*str != '\0')
-	{
-		_putchar(*str);
-		str++;
-		(*count)++;
-	}
-}
-
-/**
- * handle_format - Helper function to handle format specifiers
- * @format: Format specifier
- * @args: Variable argument list
- * @count: Pointer to the count of printed characters
- */
-void handle_format(const char *format, va_list args, int *count)
-{
-	switch (*format)
-	{
-	case 'c':
-		_putchar(va_arg(args, int));
-		(*count)++;
-		break;
-
-	case 's':
-		print_str(va_arg(args, char *), count);
-		break;
-
-	case '%':
-		_putchar('%');
-		(*count)++;
-		break;
-
-	default:
-		_putchar(*format);
-		(*count)++;
-		break;
-	}
-}
 
 /**
  * _printf - Custom printf function
@@ -69,7 +19,7 @@ int _printf(const char *format, ...)
 	{
 		if (*ptr == '%' && *(ptr + 1) != '\0')
 		{
-			handle_format(ptr + 1, args, &count);
+			count += handle_format(*(ptr + 1), args);
 			ptr++; /* Skip the next character after '%' */
 		}
 		else
@@ -80,6 +30,99 @@ int _printf(const char *format, ...)
 	}
 
 	va_end(args);
+
+	return (count);
+}
+
+/**
+ * handle_format - Helper function to handle format specifiers
+ * @specifier: Format specifier
+ * @args: Variable argument list
+ *
+ * Return: Number of characters printed
+ */
+int handle_format(char specifier, va_list args)
+{
+	int count = 0;
+
+	switch (specifier)
+	{
+	case 'c':
+		_putchar(va_arg(args, int));
+		count++;
+		break;
+
+	case 's':
+		count += print_str(va_arg(args, const char *));
+		break;
+
+	case 'd':
+	case 'i':
+		count += print_number(va_arg(args, int));
+		break;
+
+	case '%':
+		_putchar('%');
+		count++;
+		break;
+
+	default:
+		_putchar('%');
+		_putchar(specifier);
+		count += 2;
+		break;
+	}
+
+	return (count);
+}
+
+/**
+ * print_str - Helper function to print a string
+ * @str: String to print
+ *
+ * Return: Number of characters printed
+ */
+int print_str(const char *str)
+{
+	int count = 0;
+
+	if (str == NULL)
+		str = "(null)";
+
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		count++;
+		str++;
+	}
+
+	return (count);
+}
+
+/**
+ * print_number - Helper function to print a number
+ * @num: Number to print
+ *
+ * Return: Number of characters printed
+ */
+int print_number(int num)
+{
+	int count = 0;
+
+	/* Handle negative numbers */
+	if (num < 0)
+	{
+		_putchar('-');
+		count++;
+		num = -num;
+	}
+
+	/* Print digits */
+	if (num / 10 != 0)
+		count += print_number(num / 10);
+
+	_putchar(num % 10 + '0');
+	count++;
 
 	return (count);
 }
